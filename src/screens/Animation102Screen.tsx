d9fedfc4880c65c54bd4f26b7f132d0c0eabd35e
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-10 13:21:05 -0600

    feat: animated valueXY

--- src/screens/Animation102Screen.tsx
+++ src/screens/Animation102Screen.tsx
@@ -1,10 +1,35 @@
-import React from "react";
-import {StyleSheet, View} from "react-native";
+import React, {useRef} from "react";
+import {StyleSheet, View, Animated, PanResponder} from "react-native";
 
 export const Animation102Screen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event(
+      [
+        null,
+        {
+          dx: pan.x, // x,y are Animated.Value
+          dy: pan.y,
+        },
+      ],
+      {useNativeDriver: false},
+    ),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed
+        {toValue: {x: 0, y: 0}, useNativeDriver: false}, // Back to zero
+      ).start();
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <View style={styles.box} />
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[styles.box, pan.getLayout()]}
+      />
     </View>
   );
 };
@@ -12,6 +37,8 @@ export const Animation102Screen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   box: {
     width: 150,
