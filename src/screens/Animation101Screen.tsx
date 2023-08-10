6997c37cf3ebff4d0664924681f58ad995340aa8
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-10 08:19:46 -0600

    fix: FadeIn, FadeOut, Easimg y Bounce

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,8 +1,9 @@
 import React, {useRef, useEffect} from "react";
-import {StyleSheet, View, Animated, Button} from "react-native";
+import {StyleSheet, View, Animated, Button, Easing} from "react-native";
 
 export const Animation101Screen = () => {
   const opacity = useRef(new Animated.Value(0.4)).current;
+  const top = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = (callback?: Function) => {
     Animated.timing(opacity, {
@@ -10,6 +11,13 @@ export const Animation101Screen = () => {
       duration: 300,
       useNativeDriver: true,
     }).start(() => (callback ? callback() : null));
+
+    Animated.timing(top, {
+      toValue: 0,
+      duration: 800,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start();
   };
 
   const fadeOut = (duration: number = 300) => {
@@ -18,11 +26,28 @@ export const Animation101Screen = () => {
       duration,
       useNativeDriver: true,
     }).start();
+
+    Animated.timing(top, {
+      toValue: -100,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
   };
 
   return (
     <View style={styles.container}>
-      <Animated.View style={{...styles.box, opacity}} />
+      <Animated.View
+        style={{
+          ...styles.box,
+          opacity,
+          // top,
+          transform: [
+            {
+              translateY: top,
+            },
+          ],
+        }}
+      />
 
       <Button title="fadeIn" onPress={() => fadeIn()} />
       <Button title="fadeOut" onPress={() => fadeOut()} />
