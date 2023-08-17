637479f93d6cf80649929b48f16f6c33a99a544b
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-17 08:22:28 -0600

    fix: personalizando el pull to refresh

--- src/screens/PullToRefreshScreen.tsx
+++ src/screens/PullToRefreshScreen.tsx
@@ -1,10 +1,15 @@
 import React, {useState} from "react";
 import {StyleSheet, Text, View, ScrollView, RefreshControl} from "react-native";
 import {FlatListMenuHeader} from "../components/flatlist";
-import {globalStyles} from "../theme";
+import {colors, globalStyles} from "../theme";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
 
 export const PullToRefreshScreen = () => {
   const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState("");
+
+  //alternativa para solucionar el top con el notch al hacer pul to refresh
+  const {top} = useSafeAreaInsets();
 
   const onRefresh = () => {
     setRefreshing(true);
@@ -12,19 +17,41 @@ export const PullToRefreshScreen = () => {
     setTimeout(() => {
       console.log("Terminamis!!");
       setRefreshing(false);
+      setData("Hola mundo");
     }, 1500);
   };
 
   return (
     <ScrollView
+      style={{marginTop: refreshing ? top : 0}}
       refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          progressViewOffset={10} //!alto del refresh, no funciona en ios con el notch
+          progressBackgroundColor={colors.primary} //! Android
+          // colors={["white", "red", "orange"]} //! Android
+          // style={styles.background} //! estilos globales, IOS
+          // tintColor="white" //! color del spinner, IOS
+          // title="Refrehing" //! IOS
+          // titleColor="white" //! IOS
+        />
       }>
       <View style={globalStyles.margin}>
         <FlatListMenuHeader title="Pull to refresh" />
+
+        {data && <Text style={styles.title}>{data}</Text>}
       </View>
     </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    color: colors.text,
+  },
+  background: {
+    backgroundColor: colors.primary,
+  },
+});
