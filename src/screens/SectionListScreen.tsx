711edfd3494a258f9f71cd0558bd2053ec3b14bf
Jeral Pineda <jpineda@banhcafe.hn>
2023-08-17 09:05:17 -0600

    fix: header y footer de Section List

--- src/screens/SectionListScreen.tsx
+++ src/screens/SectionListScreen.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import {StyleSheet, Text, View, SectionList} from "react-native";
-import {FlatListMenuHeader} from "../components/flatlist";
-import {globalStyles} from "../theme";
+import {
+  FlatListMenuHeader,
+  FlatListMenuSeparator,
+} from "../components/flatlist";
+import {colors, globalStyles} from "../theme";
 import {casas} from "../data/section-list";
 
 export const SectionListScreen = () => {
@@ -13,12 +16,21 @@ export const SectionListScreen = () => {
         sections={casas}
         renderItem={({item}) => <Text>{item}</Text>}
         keyExtractor={(item, index) => item + index}
+        ListHeaderComponent={() => <FlatListMenuHeader title="Section List" />}
+        ListFooterComponent={() => (
+          <FlatListMenuHeader title={"Total casas " + casas.length} />
+        )}
         stickySectionHeadersEnabled
         renderSectionHeader={({section}) => (
           <View style={styles.sectionHeader}>
             <FlatListMenuHeader title={section.casa} />
           </View>
         )}
+        renderSectionFooter={({section}) => (
+          <Text style={styles.title}>{"Total: " + section.data.length}</Text>
+        )}
+        ItemSeparatorComponent={() => <FlatListMenuSeparator />}
+        showsVerticalScrollIndicator={false} //!ocultar barra scroll
       />
     </View>
   );
@@ -30,5 +42,10 @@ const styles = StyleSheet.create({
   },
   sectionHeader: {
     backgroundColor: "white", //!IOS
+  },
+  title: {
+    fontSize: 20,
+    color: colors.text,
+    marginTop: 10,
   },
 });
